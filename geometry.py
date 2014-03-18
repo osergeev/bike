9f27e8de1f1ab6c04004c1a574b6bd3648ddddf0
Oleg Sergeev <seoman@yandex.ru>
2014-03-18 14:10:50 +0100

    Getters and setters in geometry

--- geometry.py
+++ geometry.py
@@ -1,19 +1,35 @@
 from math import *
 
 class Point(object):
-	def __init__(self):
-		self._x = 0
-		self._y = 0
+	def __init__(self, x=0, y=0):
+		self._x = x
+		self._y = y
 
-	def get_coords(self):
+	def getCoords(self):
 		return (self._x, self._y)
 
-	def set_coords(self, x, y):
+	@property
+	def x(self):
+	    return self._x
+
+	@x.setter
+	def x(self, value):
+	    self._x = value
+
+	@property
+	def y(self):
+	    return self._y
+	@y.setter
+	def y(self, value):
+	    self._y = value
+
+	def setCoords(self, x, y):
 		self._x = x
 		self._y = y
 
 def dist(p1, p2):
-	xdiff = p2.get_coords[0] - p1.get_coords[0]
-	ydiff = p2.get_coords[1] - p1.get_coords[1]
+	xdiff = p2.x - p1.x
+	ydiff = p2.y - p1.y
 	return sqrt(xdiff*xdiff + ydiff*ydiff)
+
 
